7be85fad26b32f6a6c6bb51c781071b75f09215b
drvnprgrmr <ekunweibukun159357@gmail.com>
2022-12-16 16:50:51 +0100

    Added blog blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -32,9 +32,10 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
     
-    @app.route("/hello")
-    def index():
-        return "Hello, world!"
+    # Handle blog blueprint
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule("/", endpoint="index")
     
     
     return app
---
+++ flaskr/blog.py
@@ -0,0 +1,61 @@
+from flask import (
+    Blueprint, g, flash, request, redirect, render_template, url_for
+)
+
+from werkzeug.exceptions import abort
+
+
+from .auth import login_required
+from .db import get_db
+
+bp = Blueprint("blog", __name__)
+
+@bp.route("/")
+def index():
+    db = get_db()
+
+    posts = db.execute(
+        "SELECT p.id, title, body, created, author_id, username "
+        "FROM post p JOIN user u ON p.author_id = u.id "
+        "ORDER BY created DESC "
+    ).fetchall()
+
+    return render_template("blog/index.html", posts=posts)
+
+
+@bp.route("/create", methods=("GET", "POST"))
+@login_required
+def create():
+    if request.method == "POST":
+        title = request.form["title"]
+        body = request.form["body"]
+        author_id = g.user["id"]
+
+        error = None
+        if not title:
+            error = "Title is required"
+        
+        if error is not None:
+            flash(error)
+
+        db = get_db()
+        db.execute(
+            "INSERT INTO post (title, body, author_id) "
+            "VALUES (?, ?, ?)",
+            (title, body, author_id)
+        )
+        db.commit()
+
+        return redirect(url_for("blog.index"))
+    return render_template("blog/create.html")
+
+
+#TODO
+def get_post(id):
+    post = get_db().execute(
+        ""
+    )
+
+@bp.route("/<int:id>/update", methods=("GET", "POST"))
+@login_required
+def update(id): pass
